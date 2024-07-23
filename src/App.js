f1b60590e82f07104bf306ed4ad7c1c546122367
JoaoGuilhermeAval <joao.guilherme@agilean.com.br>
2024-07-23 12:06:53 -0300

    Create timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            number: 0,
+            btnText: 'GO' 
+        }
+        this.timer = null;
+        this.go = this.go.bind(this);
+        this.clean = this.clean.bind(this);
+    }
+
+    go(){
+
+        let state = this.state;
+
+        if(this.timer != null){
+            clearInterval(this.timer);
+            this.timer = null;
+            state.btnText = 'GO';
+        }else{
+            this.timer = setInterval(() =>{
+                let state = this.state;
+                state.number += 0.1;
+                this.setState(state);
+            }, 100)
+            state.btnText = 'PAUSE';
+        }
+        this.setState(state);
+    }
+
+    clean(){
+        if(this.timer != null){
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+
+        let state = this.state;
+        state.number = 0;
+        state.btnText = 'GO';
+        this.setState(state);
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <img src={require('./assets/timer.png')} className="img"/>
+                <a className="timer">{this.state.number.toFixed(1)}</a>
+                <div className="areaBtn">
+                    <a className="btn" onClick={this.go}>{this.state.btnText}</a>
+                    <a className="btn" onClick={this.clean}>CLEAN</a>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
